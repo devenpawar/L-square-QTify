0e71a5cac4f2157a2764380bd35e9d7e322baf83
Deven Pawar <devenpawar8@gmail.com>
2024-03-03 20:52:18 +0530

    Hero implementation-1

--- qtify/src/components/Hero/Hero.jsx
+++ qtify/src/components/Hero/Hero.jsx
@@ -1,16 +1,50 @@
 import React from "react";
-import styles from "./Hero.module.css";
+import "./Hero.module.css";
+import Grid from "@mui/material/Grid";
+import Headphone from "../../assets/hero.png";
 
-export default function Hero() {
-  <div className={styles.hero}>
-    <div>
-      <div>
-        <h1>100 Thousand songs, ad-free</h1>
-        <h1>Over thousand podcast episodes</h1>
-      </div>
-      <div>
-        <img src={require("../../assets/hero.png")} width={212} alt="headphone" />
-      </div>
-    </div>
-  </div>;
-}
+const Hero = () => {
+  return (
+    <Grid
+      container
+      spacing={2}
+      sx={{ backgroundColor: "black", color: "white", padding: "3rem" }}
+    >
+      <Grid
+        item
+        xs={12}
+        sm={6}
+        sx={{ display: "flex", alignItems: "flex-end", justifyContent: "flex-end" }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "end",
+            justifyContent: "end",
+            marginBottom: "1rem"
+          }}
+        >
+          <h2>100 Thousand Songs, ad-free</h2>
+          <h2>Over thousands podcast episodes”</h2>
+        </div>
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-start",
+          }}
+        >
+          <img
+            src={Headphone}
+            alt="Your"
+            style={{ width: "20%", height: "20%" }}
+          />
+        </div>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Hero;
