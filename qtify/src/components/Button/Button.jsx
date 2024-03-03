1bc4e99bba2fa266fbdd686dfd6fbff6ec8e4a91
Deven Pawar <devenpawar8@gmail.com>
2024-03-03 18:32:15 +0530

    Test-Navbar-implementation

--- qtify/src/components/Button/Button.jsx
+++ qtify/src/components/Button/Button.jsx
@@ -1,5 +1,25 @@
+// import React from "react";
+// import styles from "./Button.module.css";
+// export default function Button({ name }) {
+//   return <button className={styles.feedbackButton}>{name}</button>;
+// }
+
 import React from "react";
-import styles from "./Button.module.css";
-export default function Button({ name }) {
-  return <button className={styles.feedbackButton}>{name}</button>;
+import Button from "@mui/material/Button";
+import "./Qbutton.css";
+export default function Qbutton({ content }) {
+  return (
+    <Button
+      variant="contained"
+      className="button"
+      sx={{
+        color: "#34C94B",
+        "background-color": "#121212",
+        borderRadius: 3,
+        "font-family": "Poppins",
+      }}
+    >
+      {content}
+    </Button>
+  );
 }
